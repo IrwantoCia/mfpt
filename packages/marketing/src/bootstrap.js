a4a4f5635486f60f80b0939511ee0c143ce2a23d
irwantocia <irwantocia@gmail.com>
2021-08-29 10:57:56 +0700

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el
+    )
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root')
+    if (devRoot) {
+        mount(devRoot)
+    }
+}
+
+export { mount }
